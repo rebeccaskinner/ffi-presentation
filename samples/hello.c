9d6968600c04112bce14bf55ebf0d48dd673b55e
Rebecca Skinner <rebecca@rebeccaskinner.net>
2013-01-19 15:00:27 -0600

    add sample code and article

---
+++ samples/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* generate_message(const char* name)
+{
+    char* s = NULL;
+    asprintf(&s, "Hello, %s",name);
+    return s;
+}
+
+int main(int argc, char** argv)
+{
+    char* s = generate_message("world");
+    printf("%s\n",s);
+    free(s);
+    return EXIT_SUCCESS;
+}
